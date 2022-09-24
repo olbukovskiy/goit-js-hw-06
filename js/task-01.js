8275d9f846597d57829cfba33f517778c1f053a0
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-09-24 15:06:53 +0300

    1 task

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,8 @@
 // Напиши скрипт который:
 
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
-// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка
+// элемента(тега < h2 >) и количество элементов в категории(всех вложенных в него < li >).
 // В результате, в консоли будут выведены такие сообщения.
 
 // Number of categories: 3
@@ -14,3 +15,27 @@
 
 // Category: Technologies
 // Elements: 5
+
+const categories = document.querySelector("#categories");
+const categoriesListItems = categories.querySelectorAll(".item");
+const subcategoryListHeadings = categories.querySelectorAll(".item h2");
+
+console.log("Number of categories: ", categoriesListItems.length);
+
+
+for (let i = 0; i < subcategoryListHeadings.length; i += 1) {
+  const subcategoryListItems = categoriesListItems[i].querySelectorAll("ul li");
+
+  console.log("Category: ", subcategoryListHeadings[i].textContent);
+  console.log("Elements: ", subcategoryListItems.length);
+}
+
+
+// categoriesListItems.forEach((element) => {
+//   const listElement = element.querySelector(".item h2");
+//   const list = element.querySelector(".item ul");
+//   const listItems = list.querySelectorAll("li");
+
+//   console.log("Category: ", listElement.textContent);
+//   console.log("Elements: ", listItems.length);
+// });
