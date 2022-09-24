0022859aa07fcfbd0e22af94a64b7b60e754d453
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-09-24 22:54:35 +0300

    task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,4 +1,5 @@
-// Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать и уменьшать его значение на единицу.
+// Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать
+// и уменьшать его значение на единицу.
 
 // <div id="counter">
 //   <button type="button" data-action="decrement">-1</button>
@@ -6,6 +7,26 @@
 //   <button type="button" data-action="increment">+1</button>
 // </div>
 
-// Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
-// Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
+// Создай переменную counterValue в которой будет храниться текущее значение
+// счетчика и инициализируй её значением 0.
+// Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай
+// значение счтетчика.
 // Обновляй интерфейс новым значением переменной counterValue.
+
+const btnIncrement = document.querySelector('[data-action="increment"]');
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+let counter = document.querySelector("#value");
+let counterValue = 0;
+
+function btnIncrementFunc() {
+  counterValue += 1;
+  counter.textContent = counterValue;
+}
+
+function btnDecrementFunc() {
+  counterValue -= 1;
+  counter.textContent = counterValue;
+}
+
+btnIncrement.addEventListener("click", btnIncrementFunc);
+btnDecrement.addEventListener("click", btnDecrementFunc);
